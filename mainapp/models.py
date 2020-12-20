62418b0013a5e380060cd8f97f15608790ae7070
Sergey Zhdanov <sergeyk929@mail.ru>
2020-12-21 05:15:54 +0700

    Изображение и Media

--- mainapp/models.py
+++ mainapp/models.py
@@ -6,13 +6,16 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 User = get_user_model()
 
 class Category(models.Model):
+
     name = models.CharField(max_length=255, verbose_name="Имя категории")
     slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
+
     class Meta:
         abstract = True
 
@@ -26,7 +29,9 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
 class CartProduct(models.Model):
+
     user = models.ForeignKey("Customer", verbose_name="Пользователь", on_delete=models.CASCADE)
     cart = models.ForeignKey("Cart", verbose_name="Корзина", on_delete=models.CASCADE, related_name="related_products")
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
@@ -38,7 +43,9 @@ class CartProduct(models.Model):
     def __str__(self):
         return "Продукт: {} (для корзины)".format(self.product.title)
 
+
 class Cart(models.Model):
+
     owner = models.ForeignKey("Customer", verbose_name="Владелец", on_delete=models.CASCADE)
     products = models.ManyToManyField(CartProduct, blank=True, related_name="related_cart")
     total_products = models.PositiveIntegerField(default=0)
@@ -47,7 +54,9 @@ class Cart(models.Model):
     def __str__(self):
         return str(self.id)
 
+
 class Customer(models.Model):
+
     user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, verbose_name="Номер телефона")
     address = models.CharField(max_length=255, verbose_name="Адрес")
@@ -55,7 +64,9 @@ class Customer(models.Model):
     def __str__(self):
         return "Покупатель {} {}".format(self.first_name, self.last_name)
 
+
 class Notebook(Product):
+
     diagonal = models.CharField(max_length=255, verbose_name="Диагональ")
     display_type = models.CharField(max_length=255, verbose_name="Тип дисплея")
     processor_freq = models.CharField(max_length=255, verbose_name="Частота процессора")
@@ -66,7 +77,9 @@ class Notebook(Product):
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
 
+
 class Smartphone(Product):
+
     diagonal = models.CharField(max_length=255, verbose_name="Диагональ")
     display_type = models.CharField(max_length=255, verbose_name="Тип дисплея")
     resolution = models.CharField(max_length=255, verbose_name="Разрешение экрана")
