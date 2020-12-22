4463e326ccbb42e8b33e8a99a11a775058d4ec79
Sergey Zhdanov <sergeyk929@mail.ru>
2020-12-22 07:21:25 +0700

    Изображение ImageField

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,10 +1,29 @@
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm, ValidationError
 from django.contrib import admin
+from PIL import Image
 
 from .models import *
 
 
+class NotebookAdminForm(ModelForm):
+
+    MIN_RESOLUTION = (400, 400)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["image"].help_text = "Загружайте изображения с минимальным разрешением {}x{}".format(*self.MIN_RESOLUTION)
+
+    def clean_image(self):
+        image = self.cleaned_data["image"]
+        img = Image.open(image)
+        min_width, min_height = self.MIN_RESOLUTION
+        if img.width < min_width or img.height < min_height:
+            raise ValidationError("Разрешение загруженного изображения меньше {}x{}".format(*self.MIN_RESOLUTION))
+        return image
+
 class NotebookAdmin(admin.ModelAdmin):
+
+    form = NotebookAdminForm
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "category":
             return ModelChoiceField(Category.objects.filter(slug="notebooks"))
